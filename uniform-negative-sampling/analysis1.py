14304b1cdbba2c3f2a4136a28126ca9b79450200
Tim Poštuvan <tim.postuvan@gmail.com>
2022-05-08 23:51:54 +0200

    Update variable names

--- uniform-negative-sampling/analysis1.py
+++ uniform-negative-sampling/analysis1.py
@@ -23,18 +23,16 @@ def plot_complete_search_results(file_name, complete_search_results, edge_messag
 	
 	plt.title("AUC vs. edge message ratio")
 	plt.xlabel("Edge message ratio")
-	plt.ylabel("AUC")
-	
+	plt.ylabel("AUC")	
+
 	plt.savefig(file_name)
 	plt.clf()
-	
 
 
-def plot_difference_heatmap(file_name, plot_title, results, data_splits, adapt_epochs):
-	data_splits_labels = ["{}/{}/{}".format(int(100 * data_split[0]), int(100 * data_split[1]), 
-		int(100 * data_split[2])) for data_split in data_splits]
-	ax = sns.heatmap(100 * results, yticklabels=data_splits_labels, xticklabels=adapt_epochs, annot=True, fmt=".4g")
-	ax.set(title=plot_title, ylabel="Data split", xlabel="Adapt epochs = try epochs")
+
+def plot_difference_heatmap(file_name, plot_title, results, baseline_results, adapt_epochs, try_epochs):
+	ax = sns.heatmap(100 * (results - baseline_results), xticklabels=try_epochs, yticklabels=adapt_epochs, annot=True, fmt=".4g")
+	ax.set(title=plot_title, ylabel="Adapt epochs", xlabel="Try epochs")
 
 	plt.savefig(file_name)
 	plt.clf()
@@ -57,25 +55,24 @@ def arg_parse():
 	)
 	return parser.parse_args()
 
-
+    
 args = arg_parse()
 folder = f"{args.dataset}-{args.num_layers}-{args.hidden_dim}"
-analysis_folder = folder + "/analysis"
-
-Path(analysis_folder).mkdir(parents=True, exist_ok=True)
-
 
 data_splits = [[0.2, 0.4, 0.4], [0.5, 0.25, 0.25], [0.8, 0.1, 0.1]]
 adapt_epochs = [100, 50, 10]
+try_epochs = [1, 5, -1]
 criterions = ['val', 'gap']
 
 edge_message_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 
-best_complete_search_accs = []
 for data_split in data_splits:
 	folder_split = "{}-{}-{}".format(int(100 * data_split[0]), int(100 * data_split[1]), int(100 * data_split[2]))
 	folder_results = folder + "/results/" + folder_split
+	analysis_folder = folder + "/analysis/" + folder_split
+
+	Path(analysis_folder).mkdir(parents=True, exist_ok=True)
 
 
 	best_complete_search_val_acc = -np.inf
@@ -92,71 +89,67 @@ for data_split in data_splits:
 		if(val_acc > best_complete_search_val_acc):
 			best_complete_search_val_acc = val_acc
 			best_complete_search_test_acc = test_acc
-			
 
 	complete_search_results = np.array(complete_search_results)
-
-	best_complete_search_accs.append(best_complete_search_test_acc)
+	best_complete_search_acc = best_complete_search_test_acc
 
 
 	file_name = folder_results + "/random.csv"
 	frame = pd.read_csv(file_name)
 	random_test_acc = frame.to_numpy()[0][-1]
+	
+	print(f"Data split: {folder_split}")
+	print(f"Complete search acc: {100 * best_complete_search_acc}")
+	print(f"Random search acc: {100 * random_test_acc}")
+	print()
 
-	file_name = analysis_folder + "/complete_search_results_{}_{}_{}.png".format(int(100 * data_split[0]), 
-														int(100 * data_split[1]), int(100 * data_split[2]))
+	val_results = []
+	for adapt_epoch in adapt_epochs:
+		current_results = []
+		for try_epoch in try_epochs:
+			if(try_epoch == -1):
+				try_epoch = adapt_epoch
+
+			file_name = folder_results + "/adapt_val_{}_{}.csv".format(adapt_epoch, try_epoch)
+			frame = pd.read_csv(file_name)
+			test_acc = frame.to_numpy()[0][-1]
+			current_results.append(test_acc)
+
+		val_results.append(current_results)
+
+	val_results = np.array(val_results)
+
+
+
+	gap_results = []
+	for adapt_epoch in adapt_epochs:
+		current_results = []
+		for try_epoch in try_epochs:
+			if(try_epoch == -1):
+				try_epoch = adapt_epoch
+
+			file_name = folder_results + "/adapt_gap_{}_{}.csv".format(adapt_epoch, try_epoch)
+			frame = pd.read_csv(file_name)
+			test_acc = frame.to_numpy()[0][-1]
+			current_results.append(test_acc)
+
+		gap_results.append(current_results)
+
+	gap_results = np.array(gap_results)
+
+
+
+	file_name = analysis_folder + "/complete_search_results.png"
 	plot_complete_search_results(file_name, complete_search_results, edge_message_ratios, random_test_acc)
 
+	file_name = analysis_folder + "/difference_val_complete_search.png"
+	plot_difference_heatmap(file_name, "Difference between validation criterion and \n best complete search AUC (val - best complete search)", 
+					val_results, best_complete_search_acc, adapt_epochs, try_epochs)
 
-	
-val_results = []
-gap_results = []
-for data_split in data_splits:
-	folder_split = "{}-{}-{}".format(int(100 * data_split[0]), int(100 * data_split[1]), int(100 * data_split[2]))
-	folder_results = folder + "/results/" + folder_split
+	file_name = analysis_folder + "/difference_gap_complete_search.png"
+	plot_difference_heatmap(file_name, "Difference between gap criterion and \n best complete search AUC (gap - best complete search)", 
+					gap_results, best_complete_search_acc, adapt_epochs, try_epochs)
 
-	current_results = []
-	for adapt_epoch in adapt_epochs:
-		try_epoch = adapt_epoch
-
-		file_name = folder_results + "/adapt_val_{}_{}.csv".format(adapt_epoch, try_epoch)
-		frame = pd.read_csv(file_name)
-		test_acc = frame.to_numpy()[0][-1]
-		current_results.append(test_acc)
-
-	val_results.append(current_results)
-
-
-	current_results = []
-	for adapt_epoch in adapt_epochs:
-		try_epoch = adapt_epoch
-
-		file_name = folder_results + "/adapt_gap_{}_{}.csv".format(adapt_epoch, try_epoch)
-		frame = pd.read_csv(file_name)
-		test_acc = frame.to_numpy()[0][-1]
-		current_results.append(test_acc)
-
-	gap_results.append(current_results)
-
-
-val_results = np.array(val_results)
-gap_results = np.array(gap_results)
-
-
-
-val_complete_search_results = np.array([(val_results[i, :]) for i in range(len(val_results))])
-file_name = analysis_folder + "/difference_val_complete_search.png"
-plot_difference_heatmap(file_name, "Difference between validation criterion and \n best complete search AUC (val - best complete search)", 
-				val_complete_search_results, data_splits, adapt_epochs)
-
-
-gap_complete_search_results = np.array([(gap_results[i, :]) for i in range(len(gap_results))])
-file_name = analysis_folder + "/difference_gap_complete_search.png"
-plot_difference_heatmap(file_name, "Difference between gap criterion and \n best complete search AUC (gap - best complete search)", 
-				gap_complete_search_results, data_splits, adapt_epochs)
-
-
-val_gap_results = val_results - gap_results
-file_name = analysis_folder + "/difference_val_gap.png"
-plot_difference_heatmap(file_name, "Difference between validation and \n gap criterion AUC (val - gap)", 
-				val_gap_results, data_splits, adapt_epochs)
+	file_name = analysis_folder + "/difference_val_gap.png"
+	plot_difference_heatmap(file_name, "Difference between validation and \n gap criterion AUC (val - gap)", 
+					val_results, gap_results, adapt_epochs, try_epochs)
--- uniform-negative-sampling/analysis2.py
+++ uniform-negative-sampling/analysis2.py
@@ -1,9 +1,8 @@
 """
 Plots:
-- complete search AUC vs. edge message ratio graph
-- difference between validation criterion and best complete search AUC heatmap
-- difference between gap criterion and best complete search AUC heatmap
-- difference between validation and gap criterion AUC heatmap
+- selected edge message ratio vs. epochs during training for AdaGrid approach 
+  (on the same figure more subfigures are plotted)
+- on each subfigure also the best complete search message ratio is plotted
 """
 
 import argparse
@@ -13,29 +12,7 @@ import numpy as np
 import matplotlib.pylab as plt
 import seaborn as sns
 
-
-def plot_complete_search_results(file_name, complete_search_results, edge_message_ratios, random_test_acc):
-	random_results = [random_test_acc for x in edge_message_ratios]
-
-	plt.plot(edge_message_ratios, complete_search_results, "b", label="Complete search AUC")
-	plt.plot(edge_message_ratios, random_results, "r", label="Random approach AUC")
-	plt.legend()
-	
-	plt.title("AUC vs. edge message ratio")
-	plt.xlabel("Edge message ratio")
-	plt.ylabel("AUC")	
-
-	plt.savefig(file_name)
-	plt.clf()
-
-
-
-def plot_difference_heatmap(file_name, plot_title, results, baseline_results, adapt_epochs, try_epochs):
-	ax = sns.heatmap(100 * results, xticklabels=try_epochs, yticklabels=adapt_epochs, annot=True, fmt=".4g")
-	ax.set(title=plot_title, ylabel="Adapt epochs", xlabel="Try epochs")
-
-	plt.savefig(file_name)
-	plt.clf()
+plt.rcParams["figure.figsize"] = (12, 5)
 
 
 
@@ -59,97 +36,76 @@ def arg_parse():
 args = arg_parse()
 folder = f"{args.dataset}-{args.num_layers}-{args.hidden_dim}"
 
+iterations = 1
+num_epochs = 500
 data_splits = [[0.2, 0.4, 0.4], [0.5, 0.25, 0.25], [0.8, 0.1, 0.1]]
 adapt_epochs = [100, 50, 10]
 try_epochs = [1, 5, -1]
 criterions = ['val', 'gap']
 
-edge_message_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+complete_search_edge_message_ratios = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
 
 for data_split in data_splits:
 	folder_split = "{}-{}-{}".format(int(100 * data_split[0]), int(100 * data_split[1]), int(100 * data_split[2]))
 	folder_results = folder + "/results/" + folder_split
 	analysis_folder = folder + "/analysis/" + folder_split
-
+	
 	Path(analysis_folder).mkdir(parents=True, exist_ok=True)
 
 
 	best_complete_search_val_acc = -np.inf
-	best_complete_search_test_acc = -np.inf
-
-	complete_search_results = []
-	for edge_message_ratio in edge_message_ratios:
+	best_complete_search_message_ratio = 0.0
+	for edge_message_ratio in complete_search_edge_message_ratios:
 		file_name = folder_results + "/normal_{}.csv".format(int(100 * edge_message_ratio))
 		frame = pd.read_csv(file_name)
 		val_acc = frame.to_numpy()[0][-2]
-		test_acc = frame.to_numpy()[0][-1]
-		complete_search_results.append(test_acc)
 
 		if(val_acc > best_complete_search_val_acc):
 			best_complete_search_val_acc = val_acc
-			best_complete_search_test_acc = test_acc
-
-	complete_search_results = np.array(complete_search_results)
-	best_complete_search_acc = best_complete_search_test_acc
+			best_complete_search_message_ratio = edge_message_ratio
 
 
-	file_name = folder_results + "/random.csv"
-	frame = pd.read_csv(file_name)
-	random_test_acc = frame.to_numpy()[0][-1]
-	
-	print(f"Data split: {folder_split}")
-	print(f"Complete search acc: {100 * best_complete_search_acc}")
-	print(f"Random search acc: {100 * random_test_acc}")
-	print()
 
-	val_results = []
+	color = ["b-.", "g:", "y--"]
 	for adapt_epoch in adapt_epochs:
-		current_results = []
-		for try_epoch in try_epochs:
-			if(try_epoch == -1):
-				try_epoch = adapt_epoch
+		fig, axs = plt.subplots(1, 2)
 
-			file_name = folder_results + "/adapt_val_{}_{}.csv".format(adapt_epoch, try_epoch)
-			frame = pd.read_csv(file_name)
-			test_acc = frame.to_numpy()[0][-1]
-			current_results.append(test_acc)
+		for criterion_i in range(len(criterions)):
+			ax = axs[criterion_i]
+			epochs = np.linspace(0, num_epochs, int(num_epochs / adapt_epoch), endpoint=False)
+			complete_search_ratio = [best_complete_search_message_ratio for i in range(int(num_epochs / adapt_epoch))]
+			ax.plot(epochs, complete_search_ratio, "r", label="Best complete search")
+				
+			for try_epoch_i in range(len(try_epochs)):
+				criterion = criterions[criterion_i]
+				try_epoch = try_epochs[try_epoch_i]
 
-		val_results.append(current_results)
+				if(try_epoch == -1):
+					try_epoch = adapt_epoch
 
-	val_results = np.array(val_results)
+				file_name = folder_results + "/edge_message_ratio_adapt_{}_{}_{}.csv".format(criterion, 
+																							adapt_epoch, try_epoch)
+				frame = pd.read_csv(file_name)
+				for iteration in range(1, iterations + 1):
+					edge_message_ratios = frame["Iteration {}".format(iteration)].to_numpy()
+					epochs = np.linspace(0, num_epochs, len(edge_message_ratios), endpoint=False)
 
-
-
-	gap_results = []
-	for adapt_epoch in adapt_epochs:
-		current_results = []
-		for try_epoch in try_epochs:
-			if(try_epoch == -1):
-				try_epoch = adapt_epoch
-
-			file_name = folder_results + "/adapt_gap_{}_{}.csv".format(adapt_epoch, try_epoch)
-			frame = pd.read_csv(file_name)
-			test_acc = frame.to_numpy()[0][-1]
-			current_results.append(test_acc)
-
-		gap_results.append(current_results)
-
-	gap_results = np.array(gap_results)
+					ax.plot(epochs, edge_message_ratios, color[try_epoch_i], label="Try epochs = {}".format(try_epoch))
 
 
+				if(criterion == 'val'):
+					criterion = 'Validation'
 
-	file_name = analysis_folder + "/complete_search_results.png"
-	plot_complete_search_results(file_name, complete_search_results, edge_message_ratios, random_test_acc)
+				if(criterion == 'gap'):
+					criterion = 'Gap'
 
-	file_name = analysis_folder + "/difference_val_complete_search.png"
-	plot_difference_heatmap(file_name, "Difference between validation criterion and \n best complete search AUC (val - best complete search)", 
-					val_results, best_complete_search_acc, adapt_epochs, try_epochs)
-
-	file_name = analysis_folder + "/difference_gap_complete_search.png"
-	plot_difference_heatmap(file_name, "Difference between gap criterion and \n best complete search AUC (gap - best complete search)", 
-					gap_results, best_complete_search_acc, adapt_epochs, try_epochs)
-
-	file_name = analysis_folder + "/difference_val_gap.png"
-	plot_difference_heatmap(file_name, "Difference between validation and \n gap criterion AUC (val - gap)", 
-					val_results, gap_results, adapt_epochs, try_epochs)
+				ax.legend()
+				ax.set_title("{} criterion, adapt epochs = {}".format(criterion, adapt_epoch))
+				ax.set(xlabel="Epochs", ylabel="Edge message ratio")
+		
+		
+		file_name = analysis_folder + "/edge_message_ratios_{}.png".format(adapt_epoch)
+		plt.tight_layout()
+		plt.savefig(file_name)
+		plt.clf()
